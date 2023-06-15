b558e309b69367d7cc527765371e3225d992175e
Aspect99 <lee178lee@gmail.com>
2023-06-14 21:10:15 -0700

    fix bugs related to member not being cached

--- src/commands/leaderboard/leaderboard.js
+++ src/commands/leaderboard/leaderboard.js
@@ -9,6 +9,7 @@ const generateLeaderboard = async (guildId) => {
         ['level', 'DESC'],
         ['xp', 'DESC'],
       ],
+      limit: 10 // limit the leaderboard to top 10
     });
 
     return leaderboard;
@@ -25,11 +26,16 @@ module.exports = {
       const guildId = interaction.guild.id;
       const leaderboard = await generateLeaderboard(guildId);
 
-      let leaderboardMessage = 'ACSL Level Leaderboard(Not offical):\n';
-      leaderboard.forEach((level, index) => {
-        const member = interaction.guild.members.cache.get(level.userId);
-        leaderboardMessage += `${index + 1}. ${member.displayName} - Level ${level.level}\n`;
-      });
+      let leaderboardMessage = 'ACSL Level Leaderboard(Not official):\n';
+
+      for (let i = 0; i < leaderboard.length; i++) {
+        const member = await interaction.guild.members.fetch(leaderboard[i].userId);
+        if (member) {
+          leaderboardMessage += `${i + 1}. ${member.displayName} - Level ${leaderboard[i].level}\n`;
+        } else {
+          leaderboardMessage += `${i + 1}. Unknown User (ID: ${leaderboard[i].userId}) - Level ${leaderboard[i].level}\n`;
+        }
+      }
 
       await interaction.reply(leaderboardMessage);
     } catch (error) {
